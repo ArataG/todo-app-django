30d1dc7c4a76699f7a208cd5e11bbd2d754b69d0
Arata <mau8fg@gmail.com>
2020-10-01 10:54:39 +0900

    add add_todo function

--- todo/views.py
+++ todo/views.py
@@ -1,5 +1,18 @@
 from django.shortcuts import render
+from django.utils import timezone
+from django.http import HttpResponseRedirect
+
+from .models import Todo
+
 
 # Create your views here.
 def index(request):
-    return render(request, 'todo/index.html', {})
+    todo_items = Todo.objects.all().order_by("added_date").reverse()
+    return render(request, 'todo/index.html', {"todo_items": todo_items})
+
+def add_todo(request):
+  current_date = timezone.now()
+  content = request.POST["content"]
+  Todo.objects.create(added_date=current_date, text=content) #add database
+  todo_items = Todo.objects.all().order_by("added_date")
+  return HttpResponseRedirect("/")
